68f2e6850d01306a9ed959e3c70744da6820d296
Melo96 <54907935+Melo96@users.noreply.github.com>
2024-06-02 21:23:08 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+import io
+import base64
 import streamlit as st
 from openai import OpenAI
+from PIL import Image
+from st_multimodal_chatinput import multimodal_chatinput
 
 st.title("ChatGPT-like clone")
 
@@ -16,15 +20,38 @@ if "messages" not in st.session_state:
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
-        st.write(message["content"])
+        for item in message["content"]:
+            if item['type']=='text':
+                st.write(message["content"])
+
+with st.sidebar:
+    st.container(height=600, border=False)
+    img_file_buffer = st.file_uploader('Upload an image', type=['png', 'jpg'])
+    encoded_img = None
+    if img_file_buffer is not None:
+        image = Image.open(img_file_buffer)
+        # Convert to bytes
+        with io.BytesIO() as output:
+            image.save(output, format="JPEG")
+            image_bytes = output.getvalue()
+
+        encoded_img = base64.b64encode(image_bytes).decode('utf-8')
 
 # Accept user input
-if prompt := st.chat_input("请在这里输入消息，点击Enter发送"):
+if text := st.chat_input("请在这里输入消息，点击Enter发送"):
     # Add user message to chat history
-    st.session_state.messages.append({"role": "user", "content": prompt})
+    message = {"role": "user", "content": [{"type": "text", "text": text}]}
+    # Add uploaded image
+    if encoded_img:
+        message['content'].append({"type": "image_url",
+                                   "image_url": {
+                                                    "url": f"data:image/jpeg;base64,{encoded_img}"
+                                                }
+                                })
+    st.session_state.messages.append(message)
     # Display user message in chat message container
     with st.chat_message("user"):
-        st.write(prompt)
+        st.write(text)
 
     # Display assistant response in chat message container
     with st.chat_message("assistant"):
